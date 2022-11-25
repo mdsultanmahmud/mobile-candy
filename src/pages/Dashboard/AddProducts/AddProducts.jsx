716a74d7c0e1f76e3cb3cb2ec6ed2da95cc98d6e
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-11-25 12:29:36 +0600

    loaded category data from server

--- src/pages/Dashboard/AddProducts/AddProducts.jsx
+++ src/pages/Dashboard/AddProducts/AddProducts.jsx
@@ -1,48 +1,86 @@
 import React, { useContext } from 'react';
 import { useForm } from "react-hook-form";
+import { useNavigate } from 'react-router-dom';
 import { AuthContext } from '../../../context/AuthProvider';
+import toast from 'react-hot-toast'
 const AddProducts = () => {
-    const {user} = useContext(AuthContext)
-    const { register, handleSubmit } = useForm();
+    const { user } = useContext(AuthContext)
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const navigate = useNavigate()
     const handleAddProducts = data => {
         const date = new Date()
         const postTime = date.toLocaleTimeString()
         const postDate = date.toLocaleDateString()
         const time = date.getTime()
-        data.sellerEmail = user.email 
+        data.sellerEmail = user.email
         data.sellerName = user.displayName
         data.postDate = postDate
         data.postTime = postTime
         data.time = time
         data.isVarified = false
-        
-        console.log(data)
+
+        fetch('http://localhost:5000/products', {
+            method:'POST',
+            headers:{
+                'content-type':'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+        .then(res => res.json())
+        .then(returnData =>{
+            if(returnData.acknowledged){
+                toast.success('Your products added!!')
+                navigate('/dashboard/myProducts')
+            }
+        })
     }
     return (
         <div>
             <h3 className='text-center text-2xl font-bold text-green-500 mb-4'>Add a Product to Sell</h3>
-            <form className='grid gap-5 grid-cols-1 md:grid-cols-2' onSubmit={handleSubmit(handleAddProducts)}>
-            <input {...register('productsName', {required:'You have to input products name'})} type="text" placeholder="Products Name" className="input input-bordered input-success input-sm w-full max-w-xs" />
-            <input disabled value={user?.displayName} type="text" placeholder="Products Name" className="input input-bordered input-success input-sm w-full max-w-xs" />
-            <input disabled value={user?.email} type="text" placeholder="Products Name" className="input input-bordered input-success input-sm w-full max-w-xs" />
-            <input {...register('resalePrice', {required:'You have to input products resale price'})} type="number" placeholder="Products Resale Price" className="input input-bordered input-success input-sm w-full max-w-xs" />
-            <input {...register('originalPrice', {required:'You have to input products origina price'})} type="number" placeholder="Products Original Price" className="input input-bordered input-success input-sm w-full max-w-xs" />
-            <input {...register('sellerLocation', {required:'You have to input Your selles location'})} type="text" placeholder="Your location(Dhaka, Rajshahi)" className="input input-bordered input-success input-sm w-full max-w-xs" />
-            <input {...register('phone', {required:'You have to input your phone number'})} type="number" placeholder="+8801*********" className="input input-bordered input-success input-sm w-full max-w-xs" />
-            <input {...register('image', {required:'You have to input products image url'})} type="text" placeholder="Image URL" className="input input-bordered input-success input-sm w-full max-w-xs" />
-            <input {...register('usedTime', {required:'Enter your used time in this products'})} type="text" placeholder="Years of use" className="input input-bordered input-success input-sm w-full max-w-xs" />
-                <select className='select select-success select-sm w-full max-w-xs' {...register("condition", {required: 'Please enter prodcuts condition'})}>
+            <form className='grid gap-5 grid-cols-1 md:grid-cols-2 my-5' onSubmit={handleSubmit(handleAddProducts)}>
+                <div>
+                    <input {...register('productsName', { required: 'You have to input products name' })} type="text" placeholder="Products Name" className="input input-bordered input-success input-sm w-full max-w-xs" />
+                    {errors.productsName && <span className='text-red-500 text-sm'>{errors?.productsName?.message}</span>}
+                </div>
+                <input disabled value={user?.displayName} type="text" placeholder="Products Name" className="input input-bordered input-success input-sm w-full max-w-xs" />
+                <input disabled value={user?.email} type="text" placeholder="Products Name" className="input input-bordered input-success input-sm w-full max-w-xs" />
+                <div>
+                    <input {...register('resalePrice', { required: 'You have to input products resale price' })} type="number" placeholder="Products Resale Price" className="input input-bordered input-success input-sm w-full max-w-xs" />
+                    {errors.resalePrice && <span className='text-red-500 text-sm'>{errors?.resalePrice?.message}</span>}
+                </div>
+                <div>
+                    <input {...register('originalPrice', { required: 'You have to input products origina price' })} type="number" placeholder="Products Original Price" className="input input-bordered input-success input-sm w-full max-w-xs" />
+                    {errors.originalPrice && <span className='text-red-500 text-sm'>{errors?.originalPrice?.message}</span>}
+                </div>
+                <div>
+                    <input {...register('sellerLocation', { required: 'You have to input Your selles location' })} type="text" placeholder="Your location(Dhaka, Rajshahi)" className="input input-bordered input-success input-sm w-full max-w-xs" />
+                    {errors.sellerLocation && <span className='text-red-500 text-sm'>{errors?.sellerLocation?.message}</span>}
+                </div>
+                <div>
+                    <input {...register('phone', { required: 'You have to input your phone number' })} type="number" placeholder="+8801*********" className="input input-bordered input-success input-sm w-full max-w-xs" />
+                    {errors.phone && <span className='text-red-500 text-sm'>{errors?.phone?.message}</span>}
+                </div>
+                <div>
+                    <input {...register('image', { required: 'You have to input products image url' })} type="text" placeholder="Image URL" className="input input-bordered input-success input-sm w-full max-w-xs" />
+                    {errors.image && <span className='text-red-500 text-sm'>{errors?.image?.message}</span>}
+                </div>
+                <div>
+                <input {...register('usedTime', { required: 'Enter your used time in this products' })} type="text" placeholder="Years of use" className="input input-bordered input-success input-sm w-full max-w-xs" />
+                {errors.usedTime && <span className='text-red-500 text-sm'>{errors?.usedTime?.message}</span>}
+                </div>
+                <select className='select select-success select-sm w-full max-w-xs' {...register("condition", { required: 'Please enter prodcuts condition' })}>
                     <option value="excellent">Excellent</option>
                     <option value="good">Good</option>
                     <option value="fair">Fair</option>
                 </select>
-                <select className='select select-success select-sm w-full max-w-xs' {...register("category", {required:'Please select a category'})}>
+                <select className='select select-success select-sm w-full max-w-xs' {...register("category", { required: 'Please select a category' })}>
                     <option value="apple">Apple</option>
                     <option value="samsung">Samsung</option>
                     <option value="huawei">Huawei</option>
                 </select>
                 <textarea className="textarea textarea-success w-full" placeholder="Products Description"></textarea>
-                <input type="submit" />
+                <br />
+                <button type='submit' className="btn btn-outline btn-accent">Add Products</button>
             </form>
         </div>
     );
--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -4,7 +4,7 @@ import SingleCategory from './SingleCategory';
 const Category = () => {
     const [categories, setCategories] = useState([])
     useEffect(() =>{
-        fetch('category.json')
+        fetch('http://localhost:5000/categories')
         .then(res => res.json())
         .then(data => setCategories(data))
     } ,[])
@@ -13,7 +13,7 @@ const Category = () => {
             <h1 className='text-center font-bold text-2xl text-green-600 my-4'>Our All Categories</h1>
             <div className='grid gap-5 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
                 {
-                    categories.map(cat => <SingleCategory key={cat?._id} singleCat = {cat}></SingleCategory>)
+                    categories.map(cat => <SingleCategory key={cat._id} singleCat = {cat}></SingleCategory>)
                 }
             </div>
         </div>
